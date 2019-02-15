fb9e074ccab1cc7bfc708b270eb9607a6240e198
Yuliia Pikhterova <juliaokopna@gmail.com>
2019-02-15 12:47:25 +0200

    lesson 3

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,38 +3,52 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests {
 
-    @Test
-    public void successfulLoginTests() throws InterruptedException {///appeared after  Thread.sleep(1000);
+    WebDriver driver;
+    @BeforeMethod
+
+
+    public void beforeMethod() {
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\pikhterova_y\\IdeaProjects\\qaauto-5.02.2019\\chromedriver.exe");
         //WebDriver driver = new ChromeDriver();
-        WebDriver driver;
         driver = new ChromeDriver();
         driver.get("https://www.linkedin.com/");
+    }
 
-        WebElement putEmail = driver.findElement(By.xpath("//input[@id = \"login-email\"]"));
-        putEmail.sendKeys("vvizbor5@gmail.com");
+    @AfterMethod
+    public void afterMethod(){
+        driver.quit();
+    }
 
-        WebElement putPassword = driver.findElement(By.xpath("//input[@id = \"login-password\"]"));
-        putPassword.sendKeys("TYpochek5_" + Keys.ENTER);
+
+    @Test
+    public void successfulLoginTests() throws InterruptedException {///appeared after  Thread.sleep(1000);
+
+
+
+        WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
+        putEmailField.sendKeys("vvizbor5@gmail.com");
+
+        WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
+        putPasswordField.sendKeys("TYpochek5_" + Keys.ENTER);
 
         Thread.sleep(1000);
 
 
+        WebElement profileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
 
-        WebElement getName = driver.findElement(By.xpath("//span[@class = \"t-16 t-black t-bold\"]"));
+        Assert.assertTrue(profileMenuItem.isDisplayed(), "profileMenuItem is not displayed on Home page");
 
-        if (getName.getText().contains("Varvara")) {
-            System.out.println("searchTerm found");
-        } else {
-            System.out.println("searchTerm not found");
-        }
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home page is incorrect");
 
 
-        //driver.close();
 
 
     }
