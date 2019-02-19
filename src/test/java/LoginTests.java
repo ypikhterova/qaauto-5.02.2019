5786ce06d13758d5833d837e5982a95ae9fc8c30
Yuliia Pikhterova <juliaokopna@gmail.com>
2019-02-19 10:28:35 +0200

    lesson 4

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -28,16 +28,18 @@ public class LoginTests {
     }
 
 
-    @Test
+    @Test (priority = 1)
     public void successfulLoginTests() throws InterruptedException {///appeared after  Thread.sleep(1000);
 
 
 
         WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
-        putEmailField.sendKeys("vvizbor5@gmail.com");
+      putEmailField.sendKeys("vvizbor5@gmail.com");
+        //putEmailField.sendKeys("johndoeseleniumtest@gmail.com");
 
         WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
         putPasswordField.sendKeys("TYpochek5_" + Keys.ENTER);
+        //putPasswordField.sendKeys("johndoepassword" + Keys.ENTER);
 
         Thread.sleep(1000);
 
@@ -50,6 +52,64 @@ public class LoginTests {
 
 
 
+
+    }
+@Test (priority = 2)
+    public void LoginWrongEmail() throws InterruptedException {
+    WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
+    putEmailField.sendKeys("vvizbor51@gmail.com");
+
+    WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
+    putPasswordField.sendKeys("TYpochek5_" + Keys.ENTER);
+
+    Thread.sleep(1000);
+
+
+    WebElement wrongEmailMessage = driver.findElement(By.xpath("//div[@id=\"error-for-username\"]"));
+
+
+
+    Assert.assertEquals(wrongEmailMessage.getText(), "Hmm, we don't recognize that email. Please try again.", "Email is incorrect");
+
+}
+
+    @Test (priority = 3)
+    public void LoginInvalidEmail() throws InterruptedException {
+
+        WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
+        putEmailField.sendKeys("vvizborgmail.com");
+
+        WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
+        putPasswordField.sendKeys("TYpochek5_" + Keys.ENTER);
+
+        Thread.sleep(1000);
+
+
+        WebElement invalidEmailMessage = driver.findElement(By.xpath("//div[@id=\"error-for-username\"]"));
+
+
+
+        Assert.assertEquals(invalidEmailMessage.getText(), "Please enter a valid email address.", "Email is invalid");
+
+    }
+
+
+    @Test (priority = 4)
+    public void LoginWrongPassword() throws InterruptedException {
+
+        WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
+        putEmailField.sendKeys("vvizbor5@gmail.com");
+
+        WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
+        putPasswordField.sendKeys("TYpochek51_" + Keys.ENTER);
+
+        Thread.sleep(1000);
+
+
+        WebElement invalidPasswordMessage = driver.findElement(By.xpath("//div[@id='error-for-password']"));
+
+
+        Assert.assertEquals(invalidPasswordMessage.getText(), "Hmm, that's not the right password. Please try again or request a new one.", "Password is incorrect");
 
     }
 
