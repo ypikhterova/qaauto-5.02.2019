714ee85282280422bca54422266755d7102113ad
Yuliia Pikhterova <juliaokopna@gmail.com>
2019-02-20 10:34:30 +0200

    lesson 5

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,6 +10,7 @@ import org.testng.annotations.Test;
 public class LoginTests {
 
     WebDriver driver;
+
     @BeforeMethod
 
 
@@ -23,64 +23,46 @@ public class LoginTests {
     }
 
     @AfterMethod
-    public void afterMethod(){
+    public void afterMethod() {
         driver.quit();
     }
 
 
-    @Test (priority = 1)
+    @Test(priority = 1)
     public void successfulLoginTests() throws InterruptedException {///appeared after  Thread.sleep(1000);
 
 
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.login("vvizbor5@gmail.com", "TYpochek5_");
 
-        WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
-      putEmailField.sendKeys("vvizbor5@gmail.com");
-        //putEmailField.sendKeys("johndoeseleniumtest@gmail.com");
+        Thread.sleep(1000);
 
-        WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
-        putPasswordField.sendKeys("TYpochek5_" + Keys.ENTER);
-        //putPasswordField.sendKeys("johndoepassword" + Keys.ENTER);
+        HomePage homePage = new HomePage(driver);
+
+
+    }
+
+    @Test(priority = 2)
+    public void LoginWrongEmail() throws InterruptedException {
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.login("vvizbor51@gmail.com", "TYpochek5_");
+
 
         Thread.sleep(1000);
 
 
-        WebElement profileMenuItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
-
-        Assert.assertTrue(profileMenuItem.isDisplayed(), "profileMenuItem is not displayed on Home page");
-
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.linkedin.com/feed/", "Home page is incorrect");
+        WebElement wrongEmailMessage = driver.findElement(By.xpath("//div[@id=\"error-for-username\"]"));
 
 
-
+        Assert.assertEquals(wrongEmailMessage.getText(), "Hmm, we don't recognize that email. Please try again.", "Wrong validation message text for incorrect email");
 
     }
-@Test (priority = 2)
-    public void LoginWrongEmail() throws InterruptedException {
-    WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
-    putEmailField.sendKeys("vvizbor51@gmail.com");
 
-    WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
-    putPasswordField.sendKeys("TYpochek5_" + Keys.ENTER);
-
-    Thread.sleep(1000);
-
-
-    WebElement wrongEmailMessage = driver.findElement(By.xpath("//div[@id=\"error-for-username\"]"));
-
-
-
-    Assert.assertEquals(wrongEmailMessage.getText(), "Hmm, we don't recognize that email. Please try again.", "Email is incorrect");
-
-}
-
-    @Test (priority = 3)
+    @Test(priority = 3)
     public void LoginInvalidEmail() throws InterruptedException {
 
-        WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
-        putEmailField.sendKeys("vvizborgmail.com");
-
-        WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
-        putPasswordField.sendKeys("TYpochek5_" + Keys.ENTER);
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.login("vvizbor5gmail.com", "TYpochek5_");
 
         Thread.sleep(1000);
 
@@ -88,20 +70,16 @@ public class LoginTests {
         WebElement invalidEmailMessage = driver.findElement(By.xpath("//div[@id=\"error-for-username\"]"));
 
 
-
-        Assert.assertEquals(invalidEmailMessage.getText(), "Please enter a valid email address.", "Email is invalid");
+        Assert.assertEquals(invalidEmailMessage.getText(), "Please enter a valid email address.", "Wrong validation message text for invalid email");
 
     }
 
 
-    @Test (priority = 4)
+    @Test(priority = 4)
     public void LoginWrongPassword() throws InterruptedException {
 
-        WebElement putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
-        putEmailField.sendKeys("vvizbor5@gmail.com");
-
-        WebElement putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
-        putPasswordField.sendKeys("TYpochek51_" + Keys.ENTER);
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.login("vvizbor5@gmail.com", "TYpochek51_");
 
         Thread.sleep(1000);
 
@@ -109,7 +87,7 @@ public class LoginTests {
         WebElement invalidPasswordMessage = driver.findElement(By.xpath("//div[@id='error-for-password']"));
 
 
-        Assert.assertEquals(invalidPasswordMessage.getText(), "Hmm, that's not the right password. Please try again or request a new one.", "Password is incorrect");
+        Assert.assertEquals(invalidPasswordMessage.getText(), "Hmm, that's not the right password. Please try again or request a new one.", "Wrong validation message text for incorrect password");
 
     }
 
