e43d04f8cd6f53b0904d983439f1532498ed8ee8
Yuliia Pikhterova <juliaokopna@gmail.com>
2019-03-01 10:18:37 +0200

    lesson 7

--- src/test/java/LandingPage.java
+++ src/test/java/LandingPage.java
@@ -1,29 +1,30 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LandingPage {
-    private WebDriver driver;
 
+    private WebDriver driver;
+    @FindBy(xpath = "//input[@id = 'login-email']")
     private WebElement putEmailField;
+
+    @FindBy(xpath = "//input[@id = 'login-password']")
     private WebElement putPasswordField;
-//class constructor
+
     public LandingPage(WebDriver driver) {
         this.driver = driver;
-        initElements();
+        PageFactory.initElements(driver, this);
     }
 
-    private void initElements(){
-        putEmailField = driver.findElement(By.xpath("//input[@id = 'login-email']"));
-        putPasswordField = driver.findElement(By.xpath("//input[@id = 'login-password']"));
-    }
+    public Object login(String userEmail, String userPassword) {
 
-
-    public void login(String userEmail, String userPassword) {
         putEmailField.sendKeys(userEmail);
         putPasswordField.sendKeys(userPassword + Keys.ENTER);
+        return driver.getCurrentUrl().contains("/feed") ? new HomePage(driver) : new LoginSubmitPage(driver);
     }
+        //homePage.isPageLoaded() == true
 
     public boolean isPageLoaded() {
        return putEmailField.isDisplayed()
